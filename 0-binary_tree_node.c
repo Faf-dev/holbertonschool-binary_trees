f68e55bbdc7592da5842c11a9fd13c9f89d1d153
root <root@DESKTOP-A00A4R8.>
2024-12-09 13:59:17 +0100

    push all empty .c file, readme and binary_trees.h

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - create a binary tree node
+ * @parent: pointer to the parent node to create
+ * @value: the value to put in the new node
+ * Return: pointer to the new node, NULL if failed
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Binary Trees
+
+Joint project for Holberton School on binary trees
+
+## Authors
+
+** Salomée Perton-Couriaut **
+** Julien Noailles **
